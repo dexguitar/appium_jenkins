48d866d52bbc76a71e039266f960347c75270f00
dexguitar <stinzane@gmail.com>
2020-04-17 15:02:36 +0300

    modified tests for web

--- src/main/java/baseClass/BaseClass.java
+++ src/main/java/baseClass/BaseClass.java
@@ -3,6 +3,7 @@ package baseClass;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
@@ -13,21 +14,25 @@ import java.util.concurrent.TimeUnit;
 public class BaseClass {
 
     public static AppiumDriver<MobileElement> driver;
+
     @BeforeTest
-    @Parameters({"deviceName","udid", "platformVersion","url"})
-    public void setup(String deviceName, String udid,String platformVersion,String url) throws InterruptedException, MalformedURLException
-    {
-        DesiredCapabilities cap=new DesiredCapabilities();
+    @Parameters({"deviceName", "udid", "platformVersion", "url"})
+    public void setup(String deviceName, String udid, String platformVersion, String url) throws InterruptedException, MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("deviceName", deviceName);
         cap.setCapability("udid", udid);
-        cap.setCapability("appActivity", "com.android.calculator2.Calculator");
-        cap.setCapability("appPackage", "com.android.calculator2");
+        cap.setCapability("browserName", "Chrome");
         cap.setCapability("platformName", "Android");
         cap.setCapability("platformVersion", platformVersion);
-        driver=new AppiumDriver<MobileElement>(new URL(url), cap);
+        driver = new AppiumDriver<MobileElement>(new URL(url), cap);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Thread.sleep(5000);
 
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
     }
 
 }
--- src/test/java/testScripts/TestScript1.java
+++ src/test/java/testScripts/TestScript1.java
@@ -1,20 +1,33 @@
 package testScripts;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import baseClass.BaseClass;
-import pages.CalculatorScreen;
+
+import static org.testng.Assert.assertEquals;
 
 public class TestScript1 extends BaseClass {
 
     @Test
-    public void setup() throws InterruptedException {
+    public void simpleLoginTest() {
+        driver.get("https://jdi-testing.github.io/jdi-mobile-web/index.html");
+        driver.findElement(By.id("user-icon")).click();
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+        driver.findElement(By.id("login-button")).click();
+        assertEquals(driver.findElement(By.id("user-name")).getText(), "ROMAN IOVLEV");
+    }
 
-        CalculatorScreen cs = new CalculatorScreen(driver);
-        cs.clickButton_1();
-        cs.clickPlusButton();
-        cs.clickButton_2();
-        Thread.sleep(5000);
+    @Test
+    public void buttonPressTest() {
+        driver.findElement(By.xpath("//span[contains(text(), 'Elements')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(), 'HTML 5')]")).click();
 
+        assertEquals(driver.getTitle(), "HTML 5");
+
+        driver.findElement(By.xpath("//input[@type='button']")).click();
+
+        assertEquals(driver.switchTo().alert().getText(), "Red button");
     }
 }
