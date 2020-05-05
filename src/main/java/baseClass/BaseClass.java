65f135d3c923720c390a00c489bcd9c33b256005
dexguitar <stinzane@gmail.com>
2020-05-05 15:58:15 +0300

    refactored to work via AppiumDriverLocalService

--- src/main/java/baseClass/BaseClass.java
+++ src/main/java/baseClass/BaseClass.java
@@ -2,55 +2,54 @@ package baseClass;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
 
     public static AppiumDriver<MobileElement> driver;
+    public static AppiumDriverLocalService service;
+    public static AppiumServiceBuilder builder;
+    public static DesiredCapabilities cap;
 
-    @BeforeTest
-    @Parameters({"deviceName", "udid", "platformVersion", "url"})
-    public void setUp(String deviceName, String udid, String platformVersion, String url) throws InterruptedException, MalformedURLException {
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("deviceName", deviceName);
-        cap.setCapability("udid", udid);
+    @BeforeSuite
+    public void startServer() throws MalformedURLException {
+        cap = new DesiredCapabilities();
+        cap.setCapability("deviceName", "emulator-5554");
         cap.setCapability("browserName", "Chrome");
         cap.setCapability("platformName", "Android");
-        cap.setCapability("platformVersion", platformVersion);
-        driver = new AppiumDriver<MobileElement>(new URL(url), cap);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        Thread.sleep(5000);
+        cap.setCapability("platformVersion", "8.1");
+        cap.setCapability("chromedriverExecutable", "/Users/doglabel/Downloads/chromedriver");
 
-        // Calling Appium
-        try {
-            Process p = Runtime.getRuntime().exec(
-                    "appium --chromedriver-executable /Users/doglabel/" +
-                            "Downloads/chromedriver"
-            );
-            InputStream is = p.getInputStream();
-            int i = 0;
-            StringBuffer sb = new StringBuffer();
-            while ((i = is.read()) != -1) {
-                sb.append((char) i);
-                System.out.println(sb.toString());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        builder = new AppiumServiceBuilder();
+        builder.usingPort(4723);
+        builder.withCapabilities(cap);
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+
+        driver = new AppiumDriver<MobileElement>(
+                new URL("http://0.0.0.0:4723/wd/hub"), cap
+        );
     }
 
-    @AfterTest
-    public void tearDown() {
-        driver.quit();
+    @AfterSuite
+    public void stopServer() {
+        try {
+            driver.quit();
+            service.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
