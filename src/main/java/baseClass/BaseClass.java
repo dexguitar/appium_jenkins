d044467feaf33e0ba261c13e5a1b7a0bcb238a85
dexguitar <stinzane@gmail.com>
2020-05-06 21:18:05 +0300

    refactored to work w/out AppiumLocalDriverService

--- src/main/java/baseClass/BaseClass.java
+++ src/main/java/baseClass/BaseClass.java
@@ -2,54 +2,56 @@ package baseClass;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BaseClass {
 
     public static AppiumDriver<MobileElement> driver;
-    public static AppiumDriverLocalService service;
-    public static AppiumServiceBuilder builder;
-    public static DesiredCapabilities cap;
+    //    public static AppiumDriverLocalService service;
+//    public static AppiumServiceBuilder builder;
+    public static DesiredCapabilities caps;
 
     @BeforeSuite
     public void startServer() throws MalformedURLException {
-        cap = new DesiredCapabilities();
-        cap.setCapability("deviceName", "emulator-5554");
-        cap.setCapability("browserName", "Chrome");
-        cap.setCapability("platformName", "Android");
-        cap.setCapability("platformVersion", "8.1");
-        cap.setCapability("chromedriverExecutable", "/Users/doglabel/Downloads/chromedriver");
+        caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "8.1");
+        caps.setCapability("chromedriverExecutable",
+                Paths.get(".").toAbsolutePath() +
+                        "/src/test/resources/chromedriver"
+        );
 
-        builder = new AppiumServiceBuilder();
-        builder.usingPort(4723);
-        builder.withCapabilities(cap);
-        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
-        builder.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
-
-        service = AppiumDriverLocalService.buildService(builder);
-        service.start();
+//        builder = new AppiumServiceBuilder();
+//        builder.usingPort(4723);
+//        builder.withCapabilities(caps);
+//        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+//        builder.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+//
+//        service = AppiumDriverLocalService.buildService(builder);
+//        service.start();
 
         driver = new AppiumDriver<MobileElement>(
-                new URL("http://192.168.0.101:4723/wd/hub"), cap
+                new URL("http://192.168.0.101:4723/wd/hub"), caps
         );
     }
 
     @AfterSuite
     public void stopServer() {
-        try {
-            driver.quit();
-            service.stop();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+        driver.quit();
+//            service.stop();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
